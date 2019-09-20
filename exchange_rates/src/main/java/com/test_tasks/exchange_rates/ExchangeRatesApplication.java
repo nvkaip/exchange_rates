310435f1f50611e609f8efb6de0137719b194728
Nikolay Kaipetskyy <nvkaip@gmail.com>
2019-09-20 16:38:57 +0300

    start point

---
+++ exchange_rates/src/main/java/com/test_tasks/exchange_rates/ExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package com.test_tasks.exchange_rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesApplication.class, args);
+    }
+
+}
